331b7d657343df1eabbef65010ba68800399ec08
AnthonyIgnatyev <anton.spba@yandex.ru>
2026-02-10 18:54:00 +0300

    fix errors in get_task.go and rename files in /pkg/api

--- pkg/api/get_tasks.go
+++ pkg/api/get_tasks.go
@@ -28,14 +28,15 @@ func getTaskHandler(w http.ResponseWriter, r *http.Request) {
 	defer r.Body.Close()
 
 	if !r.URL.Query().Has("id") {
-		writeJsonError(w, "The ID parametr can't be empty", http.StatusBadRequest)
+		writeJsonError(w, "The ID parameter can't be empty", http.StatusBadRequest)
+		return
 	}
 
 	idStr := r.URL.Query().Get("id")
 
 	id, err := strconv.Atoi(idStr)
 	if err != nil {
-		writeJsonError(w, "Wrong ID format:"+err.Error(), http.StatusInternalServerError)
+		writeJsonError(w, "Wrong ID format: "+err.Error(), http.StatusBadRequest)
 		return
 	}
 
@@ -46,7 +47,7 @@ func getTaskHandler(w http.ResponseWriter, r *http.Request) {
 
 	task, err := db.GetTask(id)
 	if err != nil {
-		writeJsonError(w, "Can't get task:"+err.Error(), http.StatusInternalServerError)
+		writeJsonError(w, "Can't get task: "+err.Error(), http.StatusInternalServerError)
 		return
 	}
 
